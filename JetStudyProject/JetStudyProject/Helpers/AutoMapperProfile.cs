318ce5bd130a07caf119929322516a1bd6c12355
Andrii-Nechiporuk <73218031+Andrii-Nechiporuk@users.noreply.github.com>
2023-11-27 03:07:38 +0200

    Added missing mapping to EventFullDto

--- JetStudyProject/JetStudyProject/Helpers/AutoMapperProfile.cs
+++ JetStudyProject/JetStudyProject/Helpers/AutoMapperProfile.cs
@@ -18,6 +18,11 @@ namespace JetStudyProject.Helpers
                 .ForMember(dest => dest.FullName, from => from.MapFrom(x => x.User.Name + " " + x.User.Surname))
                 .ForMember(dest => dest.Id, from => from.MapFrom(x => x.UserId));
 
+            CreateMap<Event, EventFullDto>()
+                .ForMember(dest => dest.EventType, from => from.MapFrom(x => x.EventType.Title))
+                .ForMember(dest => dest.Lecturers, from => from.MapFrom(x => x.Lecturers.ToList()))
+                .ForMember(dest => dest.ImageSrc, from => from.MapFrom(x => Path.Combine(_server.Features.Get<IServerAddressesFeature>().Addresses.FirstOrDefault(), WebConstants.eventsImagesPath, x.Thumbnail)));
+
             CreateMap<Event, EventPreviewDto>()
                 .ForMember(dest => dest.EventType, from => from.MapFrom(x => x.EventType.Title))
                 .ForMember(dest => dest.Lecturers, from => from.MapFrom(x => x.Lecturers.ToList()))
